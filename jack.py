2f4f66c95a35992595a203be26fac5a61a24b9c2
Jack Liu <jliu168.7643@gmail.com>
2022-06-10 16:37:11 +0000

    merged into main

--- jack.py
+++ jack.py
@@ -1,9 +1,17 @@
+#!/usr/bin/env python3
+
+# def factorial(n):
+#    assert(isinstance(n, int) and n>=0)
+#    if n == 0:
+#        return 1
+#    else:
+#        return n * factorial(n-1)
+
 def factorial(n):
-    assert(isinstance(n, int) and n>=0)
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
 
 print("enter an integer to take the factorial of!")
 print(factorial(int(input())))
